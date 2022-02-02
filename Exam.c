6a39595840f4a063ae6c25865bd8d343d5b5fcc3
akashnsorte <96072586+akashnsorte@users.noreply.github.com>
2022-02-02 09:26:56 +0530

    Add files via upload

---
+++ Exam.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int data[3][3],i,j;
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("Enter array element=");
+            scanf("%d",&data[i][j]);
+        }
+    }
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("%d",data[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+}
